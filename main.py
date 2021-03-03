14cb65dd40b83da229977037979c864d1e940fe5
ziyufish <79961016+ziyufish@users.noreply.github.com>
2021-03-03 18:21:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 ''' main '''
 # 使用自建的套件 myMath 來計算使用者輸入的五個數的平均值
+import myMath.myStatistics
+number=[]
+for i in range(5):
+  
+number.append(int(input("輸入數字:")))
+
+print(number)
+print(myMath.myStatistics.myMean(number))
